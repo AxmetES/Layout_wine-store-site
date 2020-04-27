00c69f7f70ffd1457656c080dbae147c54181481
axmetES <fraktsia@gmail.com>
2020-04-28 00:53:28 +0600

    -Used os.path.join() in module file_loader for get universal directory. -Args.directory out of bounds from the func. -Renamed (key value) variables in module file_loader. -Renamed data_on_page in main.py. -Deleed print() from ifmane - file_loader.py. end

--- file_loader.py
+++ file_loader.py
@@ -1,25 +1,26 @@
 from collections import defaultdict
 import pandas as pd
+import os
 
 import argparse
 
 parser = argparse.ArgumentParser(description='wine information file')
 parser.add_argument('directory', type=str, help='Input file directory')
 args = parser.parse_args()
+directory = args.directory
 
 
-def get_load(args):
-    data = pd.read_excel(args.directory + '/wine.xlsx', force_ascii=False, na_values=['N/A', 'NA'],
+def get_load(file_directory):
+    data = pd.read_excel(os.path.join(file_directory, 'wine.xlsx'), force_ascii=False, na_values=['N/A', 'NA'],
                          keep_default_na=False)
     drinks = data.to_dict(orient='records')
 
     wines = defaultdict(list)
-    for value in drinks:
-        key = value['Категория']
-        wines[key].append(value)
-
+    for drink in drinks:
+        key = drink['Категория']
+        wines[key].append(drink)
     return wines
 
 
 if __name__ == '__main__':
-    print(get_load(args=args))
+    get_load(directory)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from file_loader import get_load, args
+from file_loader import get_load, directory
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -12,15 +12,15 @@ template = env.get_template('template.html')
 
 now = datetime.datetime.now()
 company_age = now.year - 1920
-wines = get_load(args=args)
+wines = get_load(directory)
 
-data_on_page = template.render(
+html_page = template.render(
     wines=wines,
     company_age=company_age
 )
 
 with open('index.html', 'w', encoding='utf8') as file:
-    file.write(data_on_page)
+    file.write(html_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
